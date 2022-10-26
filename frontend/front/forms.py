14d8f074630bcca59ed33a39e2063f026cccfebc
Andy Macnab <andyrobertoj@gmail.com>
2022-10-25 21:37:04 -0600

    actualizacion de frontend

---
+++ frontend/front/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+
+class FileForm(forms.Form):
+    file=forms.FileField(label="file")
+
+class CategoriaForm(forms.Form):
+    ide=forms.CharField(label="ide")
+    nombre=forms.CharField(label="nombre")
+    descripcion=forms.CharField(label="descripcion")
+    cargatrabajo=forms.CharField(label="cargatrabajo")
+    
+class ConfiguracionForm(forms.Form):
+    ide=forms.CharField(label="ide")
+    nombre=forms.CharField(label="nombre")
+    descripcion=forms.CharField(label="descripcion")
+    recursosConfiguracion=forms.CharField(label="recursosConfiguracion")
+    
+class RecursoForm(forms.Form):
+    ide=forms.CharField(label="ide")
+    nombre=forms.CharField(label="nombre")
+    abreviatura=forms.CharField(label="abreviatura")
+    metrica=forms.CharField(label="metrica")
+    tipo=forms.CharField(label="tipo")
+    valorxhora=forms.CharField(label="valorxhora")
+
+class ClienteForm(forms.Form):
+    nit=forms.CharField(label="nit")
+    nombre=forms.CharField(label="nombre")
+    usuario=forms.CharField(label="usuario")
+    clave=forms.CharField(label="clave")
+    direccion=forms.CharField(label="direccion")
+    correo=forms.CharField(label="correo")
+    
+class InstanciaForm(forms.Form):
+    ide=forms.CharField(label="ide")
+    idconfiguracion=forms.CharField(label="idconfiguracion")
+    nombre=forms.CharField(label="nombre")
+    fechainicio=forms.CharField(label="fechainicio")
+    estado=forms.CharField(label="estado")
+    fechafinal=forms.CharField(label="fechafinal")
+    
--- frontend/front/views.py
+++ frontend/front/views.py
@@ -1,3 +1,28 @@
+import json
 from django.shortcuts import render
+from .forms import FileForm, CategoriaForm
+import requests
 
 # Create your views here.
+endpoint="http://127.0.0.1:5000/"
+
+def home(request):
+    
+    return render(request, 'home.html')
+
+def add(request):
+    if request.method=='POST':
+        form=CategoriaForm(request.POST)
+        if form.is_valid():
+            json_data=form.cleaned_data
+            response=requests.post(endpoint + 'crearcategoria',json=json_data)
+            if response.ok:
+                return render(request, 'add.html',{'form':form})
+        print("Si se cumplio")
+        return render(request, 'add.html',{'form':form})
+        
+    return render(request, 'add.html')
+
+
+def load(request):
+    return render(request, 'carga.html')
