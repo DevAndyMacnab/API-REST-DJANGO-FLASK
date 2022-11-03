999cfae27ab5c1ff753f3536fc9a1e473c54fd3b
Andy Macnab <andyrobertoj@gmail.com>
2022-11-02 23:36:43 -0600

    final commit

--- backend/gestor.py
+++ backend/gestor.py
@@ -175,21 +175,6 @@ class Gestor:
         for element in consulta:
            mensaje+= "\n" + str(element.values())
                 
-            
-        msg='''
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-        '''
         pagina.insert_text(posicion,mensaje,fontsize=6)
         f.write()
         f.save("prueba.pdf",pretty=True)  
